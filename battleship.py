5d4d9e49fbec3d711ece874477e4d604db8d917b
vin33t <vineet.chd09@gmail.com>
2017-03-19 23:51:49 +0530

    print_board, random_row, random_col defined

--- battleship.py
+++ battleship.py
@@ -2,3 +2,27 @@ from random import randint
 
 from pip._vendor.distlib.compat import raw_input
 
+board = []
+
+for x in range(5):
+    board.append(["O"] * 5)
+
+
+def print_board(board):
+    for row in board:
+        print
+        " ".join(row)
+
+
+print
+"Let's play Battleship!"
+print_board(board)
+
+
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+
