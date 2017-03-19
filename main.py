4cae23b4d39a1717b2043b2c213db5dbb2491307
vin33t <vineet.chd09@gmail.com>
2017-03-20 04:14:50 +0530

    Main menu added so that other three files can be accessed from one place

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+#printing the available options
+print("Enter the file Number you want to run")
+print("Enter 1 for Students to Teacher")
+print("Enter 2 for Battleship")
+print("Enter 3 for Exam Statistics")
+option_selected_by_user=int(input("I want to Run: "))
+
+#redirecting the user based on the input
+if option_selected_by_user==1:
+    print("You selected Student to Teacher as your Option")
+    os.system('python students_to_teacher.py')
+elif option_selected_by_user==2:
+    print("You Selected Battleship as your Option")
+    os.system('python battleship.py')
+elif option_selected_by_user==3:
+    print("You selected Exam Statistics as your Option")
+    os.system('python exam_stats.py')
+else:
+    os.system('python main.py')
